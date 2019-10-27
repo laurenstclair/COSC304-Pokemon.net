b5f4b34844375bc7567d5ee3d1d6f93932b429d8
Kathryn Lecha <43145003+kzlecha@users.noreply.github.com>
2019-10-26 17:37:02 -0700

    added foreign keys to tables

--- Lab5DDL.sql
+++ Lab5DDL.sql
@@ -7,8 +7,8 @@ create table Customer (
 	address varchar(30),
 	city varchar(30),
 	provice_state varchar(30),
-	postal_code int,
-	country varchar(20), #should we use country code or string
+	postal_code varchar(6),
+	country varchar(20),
 	user_id varchar(25),
 	password varchar(30),
 	primary key (id)
@@ -16,23 +16,30 @@ create table Customer (
 
 create table PaymentMethod(
 	id int not null auto_increment,
-	method varchar(25),
+	method varchar(),
 	num int,
 	expiratory_date datetime,
 	primary key (id)
+);
 
+create table CustomerPaymentMethod(
+	customer_id int,
+	payment_method_id int,
+	primary key(customer_id, payment_method_id)
 );
 
 create table OrderSummary(
 	id int not null auto_increment,
+	customer_id int,
 	order_date datetime,
 	total_order_amount decimal(10, 2), 
 	shipment_address varchar(40),
 	city varchar(30),
 	provice_state varchar(30),
-	postal_code int,
+	postal_code varchar(6),
 	country varchar(20),
-	primary key (id)
+	primary key (id),
+	foreign key (customer_id) references Customer(id)
 );
 
 create table Category(
@@ -43,20 +50,35 @@ create table Category(
 
 create table Product(
 	id int not null auto_increment,
+	category_id int,
 	name varchar(30),
 	price decimal(10, 2),
 	image_url varchar(100),
 	image BLOB,
 	description CLOB,
-	primary key (id)
+	primary key (id),
+	foreign key (category_id) references Category(id)
 
+);
+
+create table ProductOrder(
+	product_id int not null,
+	order_id int not null,
+	quantity int,
+	primary key (product_id, order_id),
+	foreign key (product_id) references Product (id),
+	foreign key (order_id) references Order (id)
 );
 
 create table Shipment(
 	id int not null auto_increment,
+	warehouse_id int,
+	order_id int,
 	shipment_date datetime,
 	description CLOB,
-	primary key(id);
+	primary key(id),
+	foreign key (warehouse_id) references Warehouse(id),
+	foreign key (order_id) references Order(id)
 );
 
 create table Warehouse (
@@ -64,16 +86,33 @@ create table Warehouse (
 	name varchar(40)
 );
 
+CREATE TABLE Inventory(
+	warehouse_id int not null,
+	product_id int not null,
+	quantity int,
+	primary key (warehouse_id, product_id),
+	foreign key (warehouse_id) references Warehouse (id),
+	foreign key (product_id) references Product(id)
+);
+
 create table ShoppingCart(
+	customer_id int not null,
+	product_id int not null,
 	quantity int, 
-	price decimal(10, 2)
+	price decimal(10, 2),
+	primary key (product_id, customer_id),
+	foreign key (customer_id) references Customer(id),
+	foreign key (product_id) references Product(id)	
 );
 
 create table Review(
 	id int not null auto_increment,
+	customer_id int not null,
+	product_id int not null,
 	rating decimal(1,1),
 	comment varchar(128),
 	review_date datetime,
-	primary key (id)
+	primary key (id),
+	foreign key (customer_id) references Customer(id),
+	foreign key (product_id) references Product(id)
 );
-
